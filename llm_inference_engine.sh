d93c4f939553bb3b08685d8d6e3b539cd3cb61ce
seokjeongeum <jeseok@dblab.postech.ac.kr>
2024-12-09 06:42:05 +0000

    Demo

---
+++ llm_inference_engine.sh
@@ -0,0 +1,14 @@
+export CUDA_VISIBLE_DEVICES=2,3
+docker run --gpus all \
+    -p 30000:30000 \
+    --env "HF_TOKEN=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" \
+    --ipc=host \
+    --mount type=bind,source=/mnt,target=/mnt \
+    lmsysorg/sglang:latest \
+    python3 -m sglang.launch_server --model-path /mnt/sde/shpark/models/hugging-quants/Meta-Llama-3.1-70B-Instruct-GPTQ-INT4 --host 0.0.0.0 --port 30000 --tp 1
+    
+    # --dp 1
+# /mnt/sde/shpark/models/mistralai/Ministral-8B-Instruct-2410 (75, 150, 200), # (50, 100)
+# /mnt/sde/shpark/models/google/gemma-2-9b-it (75, 150, 200), # (50, 100)
+# /mnt/sde/shpark/models/Qwen/Qwen2.5-7B-Instruct #(75, 150, 200), # (50, 100)
+# --kv-cache-dtype fp8_e5m2 --context-length 6400 
