a472ffc755f84011d1bb4b0fcfcea5033a76e70f
석정음 SEOK Jeongeum <jeseok@dblab.postech.ac.kr>
2024-12-09 21:14:06 +0900

    Fix paths

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SQLBot
+export PYTHONPATH=.:third_party/nl2qgm:third_party/nl2qgm/ratsql/resources
+CUDA_VISIBLE_DEVICES=1 python third_party/nl2qgm/demo/backend_server.py
--- llm_inference_engine.sh
+++ llm_inference_engine.sh
@@ -1,11 +1,11 @@
-export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=0
 docker run --gpus all \
     -p 30000:30000 \
     --env "HF_TOKEN=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" \
     --ipc=host \
     --mount type=bind,source=/mnt,target=/mnt \
     lmsysorg/sglang:latest \
-    python3 -m sglang.launch_server --model-path /mnt/sde/shpark/models/hugging-quants/Meta-Llama-3.1-70B-Instruct-GPTQ-INT4 --host 0.0.0.0 --port 30000 --tp 1
+    python3 -m sglang.launch_server --model-path /mnt/sdc/jeseok/Meta-Llama-3.1-70B-Instruct-GPTQ-INT4 --host 0.0.0.0 --port 30000 --tp 1
     
     # --dp 1
 # /mnt/sde/shpark/models/mistralai/Ministral-8B-Instruct-2410 (75, 150, 200), # (50, 100)
