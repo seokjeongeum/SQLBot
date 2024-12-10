79ec218568ef79e7945f766670129dcaa74af3f5
seokjeongeum <jeseok@dblab.postech.ac.kr>
2024-12-10 00:58:40 +0000

    Fix devcontainer

--- query.sh
+++ query.sh
@@ -1,3 +1,3 @@
-curl -X POST http://localhost:7000/text_to_sql \
+curl -X POST http://141.223.197.39:7000/text_to_sql \
 -H "Content-Type: application/json" \
 -d '{"text": "how many singer?", "db_id": "concert_singer", "analyse": "true", "reset_history": "false"}'
